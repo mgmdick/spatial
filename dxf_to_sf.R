23bfa112171245e87d3aaf0d0bf4989b80306a4e
Matthew Dick <mgmdick@gmail.com>
2019-06-14 13:25:53 +1000

    Update dxf_to_sf.R
    Added code to remove non-vertex geometries

--- dxf_to_sf.R
+++ dxf_to_sf.R
@@ -1,44 +1,40 @@
-library(sp)
-library(sf)
-library(ggplot2)
-library(leaflet)
-library(leaflet.extras)
 
-#add path here
-path <- _________
+dxf_to_sf <- function(path) {
 
-#read as lines
-dxf <- readLines(path)
+dxf = readLines(path)
 
-#set the original CRS
-crs_string <- _________
+crs_string <- "+init=epsg:28355"
 
-#enter the string which tags the object name, or a string that is contained in the object names consistently
-header_tag <- _________
 #find the section breaks
 section_breaks <- which(dxf == 1001)
 
 #get the index of the lines for the polygon names
-section_names <- which(stringr::str_detect(dxf, header_tag))
+section_names <- which(stringr::str_detect(dxf, "VulcanName="))
 
 #strip extra characters for nicer names
-poly_names <- str_replace_all( dxf[section_names], header_tag, "")
+poly_names <- str_replace_all( dxf[section_names], "VulcanName=", "")
 
 #create a table of section start and end lines to pull the coordinates
 sections <- data.frame(id_start = section_names, id_end = data.table::shift(section_names, type = "lead") - 1, names = poly_names)
 
 #end of last section is given by length of file
 sections$id_end[nrow(sections)] <- length(dxf)
+
 sections <- sections[sections$names != "",]
+
 #for each row in the section
 sf_polygons <- lapply(1:nrow(sections), function(i) {
-
+  i = 7
   #get the lines relevant to the section
   section_lines <- dxf[sections[i,1] : sections[i,2]]
   
   #get the indices of the " 10" and " 20" rows, and add one to the index to drop down a line
   x_lines <- which(section_lines == " 10") + 1
   y_lines <- which(section_lines == " 20") + 1
+  
+  
+  x_lines <- x_lines[section_lines[x_lines - 6] == "VERTEX"]
+  y_lines <- y_lines[section_lines[y_lines - 8] == "VERTEX"]
   
   #get the values and cast to numeric
   y <- as.numeric(section_lines[y_lines])
@@ -56,5 +52,7 @@ sf_polygons <- lapply(1:nrow(sections), function(i) {
 
 
 all_shapes <- do.call(rbind, sf_polygons)
+all_shapes 
 
-ggplot(all_shapes) + geom_sf()
+}
+
